68eddc7bbfaac2e71031cc9073aa2a78ec7ea108
Hoarau Erwan <ehoarau003@iutbayonne.univ-pau.fr>
2026-09-23 14:50:27 +0200

    9 test paramétré pour add

--- src/test/java/com/erwan/calculator/CalculatorTest.java
+++ src/test/java/com/erwan/calculator/CalculatorTest.java
@@ -1,6 +1,8 @@
 package com.erwan.calculator;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -17,6 +19,22 @@ class CalculatorTest {
 
         // THEN
         assertThat(somme).isEqualTo(3);
+    }
+
+    @ParameterizedTest(name = "{0} + {1} = {2}")
+    @CsvSource({
+            "0,  1,  1",
+            "1,  2,  3",
+            "-2, 2,  0",
+            "0,  0,  0",
+            "-1, -2, -3"
+    })
+    void add_devrait_calculer_la_somme_pour_plusieurs_valeurs(int opG, int opD, int resultatAttendu) {
+        // WHEN
+        int somme = Calculator.add(opG, opD);
+
+        // THEN
+        assertThat(somme).isEqualTo(resultatAttendu);
     }
 
     @Test
